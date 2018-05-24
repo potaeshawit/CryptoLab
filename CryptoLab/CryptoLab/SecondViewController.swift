aae32b97a2e7c499cede54eb77d95950adccf5fc
Taeshawit Suwanwigo <s.taeshawit@icloud.com>
2018-05-24 19:09:10 +0700

    [init]

---
+++ CryptoLab/CryptoLab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CryptoLab
+//
+//  Created by Taeshawit Suwanwigo on 24/5/2561 BE.
+//  Copyright © 2561 Taeshawit Suwanwigo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
