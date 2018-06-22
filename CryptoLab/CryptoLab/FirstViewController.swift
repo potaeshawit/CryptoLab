1dc0594ca31a2d6797c54231182bc78701fbfa51
Taeshawit Suwanwigo <s.taeshawit@icloud.com>
2018-06-22 12:40:39 +0700

    [add] dev

--- CryptoLab/CryptoLab/FirstViewController.swift
+++ CryptoLab/CryptoLab/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("changes on develop branch")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
